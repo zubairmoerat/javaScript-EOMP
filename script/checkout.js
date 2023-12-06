b6016cf825f350130ca47d35f4ad1a1577367ff6
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-12-06 12:28:13 +0200

    finishPageLater

--- script/checkout.js
+++ script/checkout.js
@@ -1,3 +1,56 @@
 //adding and removing objects from an empty array
 //footer year
 document.querySelector('#currYear').textContent = new Date().getFullYear()
+
+let checkouts = JSON.parse(localStorage.getItem('checkout')) 
+let checkoutTable = document.querySelector('[data-checkout]')
+function cartItems(){
+    checkoutTable.innerHTML = ""
+    if(checkouts){
+        checkouts.forEach((checkout, i)=>{
+            checkoutTable.innerHTML +=`
+            <table class="table table-bordered">
+               <thead>
+                    <tr>
+                        <th scope="col">Product</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Amount</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr>
+                        <td><img src="${checkout.image}" class="img-thumbnail"> ${checkout.name}</td>
+                        <td>${checkout.quantity}</td>
+                        <td>${checkout.amount}</td>
+                    </tr>
+                </tbody>
+                <tfoot class="table-group-divider">
+                    <tr>
+                        <td><a class="btn btn-secondary" id="pay" onclick='productPayment()'>Pay Now</a></td>
+                        <td>Total Quantity:</td>
+                        <td>Total Amount:</td>
+                    </tr>
+                </tfoot>
+            </table>
+            `
+        })
+    }else{
+        checkoutTable.innerHTML = `
+        <table class="table table-bordered">
+               <thead>
+                    <tr>
+                        <th scope="col">Product</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Amount</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr>
+                        <td>Please add products</td>
+                    </tr>
+                </tbody>
+            </table>
+        `
+    }
+}
+cartItems()
--- script/product.js
+++ script/product.js
@@ -9,70 +9,80 @@ localStorage.setItem('products', JSON.stringify([
         name: 'Protein Powder',
         image: "https://i.postimg.cc/RhfYmFwP/js-EOMP-prod1.jpg",
         detail: '24 grams of protein with low levels of fat and 120 calories.',
-        amount: 'R1000'
+        amount: 'R1000',
+        quantity: 1
     },
     {
         id: 2,
         name: 'Gym Bag',
         image: "https://i.postimg.cc/sgmsbVPP/js-EOMP-prod2.jpg",
         detail: 'For carrying your gym essentials.',
-        amount: 'R1500'
+        amount: 'R1500',
+        quantity: 1
     },
     {
         id: 3,
         name: 'Lifting Belt',
         image: "https://i.postimg.cc/J4H7FVtt/js-EOMP-prod3.jpg",
         detail: 'Offers support to the lower back during heavy compound lifts.',
-        amount: 'R800'
+        amount: 'R800',
+        quantity: 1
     },
     {
         id: 4,
         name:'Fitness Tracker',
         image: "https://i.postimg.cc/BZN5Zz5q/js-EOMP-prod4.jpg",
         detail: 'Monitors and tracks your fitness metrics.',
-        amount: 'R2500'
+        amount: 'R2500',
+        quantity: 1
     },
     {
         id: 5,
         name: 'Creatine',
         image: "https://i.postimg.cc/JzWR6R0x/js-EOMP-prod5.jpg",
         detail: 'Increase performance during high intensity training.',
-        amount: 'R600'
+        amount: 'R600',
+        quantity: 1
     },
     {
         id: 6,
         name: 'Trenbolone',
         image:"https://i.postimg.cc/RFQ1Cdrk/js-EOMP-prod6.jpg",
         detail: 'Increases muscle growth and appetite',
-        amount: 'R5000'
+        amount: 'R5000',
+        quantity: 1
     },
     {
         id: 7,
         name: 'Compression Sweater',
         image: "https://i.postimg.cc/9Fjkm86f/js-EOMP-prod7.jpg",
         detail: 'Perfect material to control body temperature.',
-        amount: 'R500'
+        amount: 'R500',
+        quantity: 1
     },
     {
         id: 8,
         name: 'Boxing Gloves',
         image: "https://i.postimg.cc/MHRr9jv6/js-EOMP-prod-UP.jpg",
         detail: 'Combines durability and comfort for the ultimate training experience.',
-        amount: 'R900'
+        amount: 'R900',
+        quantity: 1
     },
     {
         id: 9,
         name: 'Water Bottle',
         image: "https://i.postimg.cc/bJXzr1c8/js-EOMP-prod9.jpg",
         detail: 'Great for on-the-go hydration and supplementation.',
-        amount: 'R150'
+        amount: 'R150',
+        quantity: 1
     },
     {
         id: 10,
         name: 'Headphones',
         image: "https://i.postimg.cc/VLS0JB20/js-EOMP-prod10.jpg",
         detail: 'Noise cancelation and comfortable fit for an emmersive experience.',
-        amount: 'R3500'
+        amount: 'R3500',
+        quantity: 1
     }
 ]))
 let purchased = []
@@ -89,7 +99,7 @@ function displayProducts(){
               <h5 class="card-title">${product.name}</h5>
               <p class="card-text">${product.detail}</p>
               <p class="card-text">${product.amount}</p>
-              <a href="#" class="btn btn-secondary" id="cart" onclick='addToCart(${JSON.stringify(product)})'>Add To Cart</a>
+              <a href="/html/checkout.html" class="btn btn-secondary" id="cart" onclick='addToCart(${JSON.stringify(product)})'>Add To Cart</a>
             </div>
             </div>
             </div>
@@ -118,7 +128,7 @@ productSearch.addEventListener('keyup', ()=>{
               <h5 class="card-title">${item.name}</h5>
               <p class="card-text">${item.detail}</p>
               <p class="card-text">${item.amount}</p>
-              <a href="#" class="btn btn-secondary" id="cart" onclick='addToCart(${JSON.stringify(item)})'>Add To Cart</a>
+              <a href="/html/checkout.html" class="btn btn-secondary" id="cart" onclick='addToCart(${JSON.stringify(item)})'>Add To Cart</a>
             </div>
             </div>
             </div>
@@ -160,7 +170,7 @@ productSort.addEventListener('click', function(){
           <h5 class="card-title">${item.name}</h5>
           <p class="card-text">${item.detail}</p>
           <p class="card-text">${item.amount}</p>
-          <a href="#" class="btn btn-secondary" id="cart" onclick='addToCart(${JSON.stringify(item)})>Add To Cart</a>
+          <a href="/html/checkout.html" class="btn btn-secondary" id="cart" onclick='addToCart(${JSON.stringify(item)})>Add To Cart</a>
         </div>
         </div>
         </div>
